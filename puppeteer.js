bb8ba47bbdfd86edae2bb5a0f95b2b2893ee4ea9
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-11-26 20:19:21 -0500

    fix: move pointer to the correct position

--- puppeteer.js
+++ puppeteer.js
@@ -16,7 +16,7 @@ if (!destination) {
   })
   const page = await browser.newPage()
   await page.setViewport({ width: 1500, height: 1080 })
-  await page.goto(url, { waitUntil: 'domcontentloaded' })
+  await page.goto(url, { waitUntil: 'load' })
   await page.screenshot({
     path: destination
   })
