5adebcb60b9fd3d69ed786b2a7bc29385063a433
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-12-04 17:49:43 -0500

    fix(tree.sh): fix tree.sh

--- tree.sh
+++ tree.sh
@@ -3,5 +3,5 @@
 #File: tree2githubmd
 #Description: Convert output of unix tree utility to Github flavoured Markdown
 
-tree="$(tree -f --noreport --charset ascii $1 | sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g')"
+tree="$(tree -f --noreport --charset ascii "$1" | sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g')"
 printf "# Code/Directory Structure:\n%s\n" "${tree}"
