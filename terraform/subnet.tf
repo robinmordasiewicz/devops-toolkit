6423616cebba74794e592ea81866512ae0f813c4
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-11 11:23:15 -0400

    This is a Terraform configuration file for creating an Azure Resource Group, a subnet within it, a Network Security Group with two rules denying all incoming and outgoing traffic, associating the network security group to the subnet, and creating a Ubuntu virtual machine with a static IP address in the subnet and connecting it to the network security group.
    The configuration file uses the `azurerm` provider version 3.97.1 and includes variables for the resource group name prefix and the Ubuntu VM username. The `azapi` provider is also included with version 1.5, but it's not used in this example.
    
    The configuration file defines an output for the resource group name.
    
    The `ubuntu-outputs.tf` file outputs the resource group name.
    
    The `ubuntu-variables.tf` file sets the default values for the resource group name prefix and Ubuntu VM username.
    
    The `ubuntu.tf` file creates a network interface, associates it with a security group, and creates a Ubuntu virtual machine with a static IP address in the subnet and connects it to the network security group. The virtual machine's admin SSH key is specified using a random pet name and an RSA private key generated by Terraform.
    
    The `dmz_prefix` variable is not defined in this example, so the fifth IP address of the subnet (`cidrhost(var.dmz_prefix, 5)`) will result in an error. You should define the `dmz_prefix` variable and set it to a valid CIDR block that includes at least five available IP addresses for the network interface and the virtual machine.

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,31 +1,3 @@
-resource "azurerm_network_security_group" "private_nsg" {
-  name                = "private_nsg"
-  location            = azurerm_resource_group.azure_resource_group.location
-  resource_group_name = azurerm_resource_group.azure_resource_group.name
-  security_rule {
-    name                       = "block-all-in"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Deny"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "block-all-out"
-    priority                   = 101
-    direction                  = "Outbound"
-    access                     = "Deny"
-    protocol                   = "*"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
 resource "azurerm_subnet_network_security_group_association" "external_subnet_association" {
   subnet_id                 = azurerm_subnet.external_subnet.id
   network_security_group_id = azurerm_network_security_group.private_nsg.id
---
+++ terraform/ubuntu.tf
@@ -0,0 +1,51 @@
+# Create network interface
+resource "azurerm_network_interface" "ubuntu_nic" {
+  name                = "ubuntuNIC"
+  location            = azurerm_resource_group.azure_resource_group.location
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
+
+  ip_configuration {
+    name                          = "ubuntu-dmz-ipconfig"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = cidrhost(var.dmz_prefix, 5)
+    subnet_id                     = azurerm_subnet.dmz_subnet.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.ubuntu_nic.id
+  network_security_group_id = azurerm_network_security_group.private_nsg.id
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "ubuntu_vm" {
+  name                  = "ubuntuVM"
+  location              = azurerm_resource_group.azure_resource_group.location
+  resource_group_name   = azurerm_resource_group.azure_resource_group.name
+  network_interface_ids = [azurerm_network_interface.ubuntu_nic.id]
+  size                  = "Standard_DS1_v2"
+
+  admin_ssh_key {
+    username   = random_pet.admin_username.id
+    public_key = tls_private_key.ssh_key.public_key_openssh
+    #public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    name                 = "ubuntuOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  computer_name  = "ubuntu"
+  admin_username = random_pet.admin_username.id
+
+}
