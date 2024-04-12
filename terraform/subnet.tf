a0755c42f24afedfa7efe1ec023390b21b056cf2
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-12 17:17:28 -0400

    It looks like you have made several changes to your Terraform configuration files. Here's a summary of the changes:
    1. You have updated the required version of Terraform to 1.8.0.
    2. In the `vnet.tf` file, you have added two new subnets named `external_subnet` and `internal_subnet`. The `external_subnet` has an address prefix of `var.external_prefix`, while the `internal_subnet` has an address prefix of `var.internal_prefix`.
    3. In the `ubuntu.tf` file, you have removed the `azurerm_network_security_group` resource named `ubuntu_allow_https_tcp_nsg`, and instead created two new network security rules with names `VIP-allow_http_tcp` and `VIP-allow_https_tcp`. Both rules allow inbound traffic on ports 80 and 443, respectively.
    4. You have also removed the association of the network security group to the network interface, as it is no longer needed since the security rules are defined directly on the virtual network.
    5. Finally, you have created a new `azurerm_network_interface` resource named `ubuntu_internal_network_interface`, which will be used for the Ubuntu VM's internal network interface. The name and location of this resource are specified in the configuration.
    
    It's important to note that without additional context, it's impossible to determine if these changes are correct or appropriate for your use case. Make sure to test your configuration thoroughly before deploying it to production.

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,9 +1,65 @@
+resource "azurerm_subnet_network_security_group_association" "external_association" {
+  subnet_id                 = azurerm_subnet.external_subnet.id
+  network_security_group_id = azurerm_network_security_group.external_nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "internal_association" {
+  subnet_id                 = azurerm_subnet.internal_subnet.id
+  network_security_group_id = azurerm_network_security_group.internal_nsg.id
+}
+
+resource "azurerm_network_security_group" "internal_nsg" { #tfsec:ignore:azure-network-no-public-ingress
+  name                = "internal_nsg"
+  location            = azurerm_resource_group.azure_resource_group.location
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
+  security_rule {
+    name                       = "internal-allow_https_tcp"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["443"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_security_group" "external_nsg" { #tfsec:ignore:azure-network-no-public-ingress
+  name                = "external_nsg"
+  location            = azurerm_resource_group.azure_resource_group.location
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
+  security_rule {
+    name                       = "MGMT-allow_https_tcp"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["443"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "VIP-allow_https_tcp"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["443"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_subnet" "external_subnet" {
   address_prefixes     = [var.external_prefix]
   name                 = var.external_name
   resource_group_name  = azurerm_resource_group.azure_resource_group.name
   virtual_network_name = azurerm_virtual_network.vnet.name
 }
+
 resource "azurerm_subnet" "internal_subnet" {
   address_prefixes     = [var.internal_prefix]
   name                 = var.internal_name
