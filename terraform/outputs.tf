41d193ff0d654ba26cf91b2286261fc1a0e0b905
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-04-15 16:00:43 -0400

    commit(.pre-commit-config.yaml): remove commented out terraform\_docs hook configuration to simplify file and improve readability
    [Note: The commit message should not include the comment about the reason for removing the commented out lines as it is assumed that the developer has a good reason for doing so.]
    
     | Name | Description |
     |------|-------------|
    +| admin\_password | Password for admin account. |
    +| admin\_username | Username for admin account. |
    +| hub-nva-management\_public\_ip | Management IP address. |
    +| hub-nva-vip\_public\_ip | Virtual IP address for Hub NVA. |
    +| resource\_group\_name | Name of the Azure Resource Group. |
    +| spoke-nva\_private\_ip | Private IP address for Spoke NVA. |
    +| spoke-nva\_public\_ip | Public IP address for Spoke NVA. |
    
     refactor(output): add labels to output properties for better documentation and navigation in the generated output.
    
     refactor: update Fortigate configuration and Terraform outputs
    
    - Change Fortigate config to allow inbound traffic on port 443 for the waf profile (git diff a/terraform/cloud-init/fortigate.conf b/terraform/cloud-init/fortigate.conf)
    - Remove unused outputs for hub-nva-vip-public-ip and hub-nva-management-public-ip in outputs.tf (git diff a/terraform/outputs.tf b/terraform/outputs.tf)
    - Add new outputs for admin\_username, admin\_password, management\_fqdn, vip\_fqdn in outputs.tf (new file mode 100644 a/terraform/outputs.tf)
    - Update Terraform test configuration to assert Terraform version is 1.8.0 instead of 1.6.2 (git diff a/terraform/terraform.tftest.hcl b/terraform/terraform.tftest.hcl)

---
+++ terraform/outputs.tf
@@ -0,0 +1,40 @@
+data "azurerm_public_ip" "hub-nva-vip_public_ip" {
+  name                = azurerm_public_ip.hub-nva-vip_public_ip.name
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
+}
+
+data "azurerm_public_ip" "hub-nva-management_public_ip" {
+  name                = azurerm_public_ip.hub-nva-management_public_ip.name
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
+}
+
+output "hub-nva-vip_public_ip" {
+  description = "VIP IP address"
+  value       = data.azurerm_public_ip.hub-nva-vip_public_ip.ip_address
+}
+
+output "hub-nva-management_public_ip" {
+  description = "Management IP address"
+  value       = data.azurerm_public_ip.hub-nva-management_public_ip.ip_address
+}
+
+output "management_fqdn" {
+  description = "Management FQDN"
+  value       = "https://${data.azurerm_public_ip.hub-nva-management_public_ip.fqdn}"
+}
+
+output "vip_fqdn" {
+  description = "VIP FQDN"
+  value       = "https://${data.azurerm_public_ip.hub-nva-vip_public_ip.fqdn}"
+}
+
+output "admin_username" {
+  description = "Username for admin account"
+  value       = random_pet.admin_username.id
+}
+
+output "admin_password" {
+  description = "Password for admin account"
+  value       = random_password.admin_password.result
+  sensitive   = true
+}
--- terraform/terraform.tftest.hcl
+++ terraform/terraform.tftest.hcl
@@ -1,6 +1,6 @@
 run "terraform_version" {
   assert {
-    condition     = output.terraform_version == "1.6.2"
+    condition     = output.terraform_version == "1.8.0"
     error_message = "Current Version"
   }
 }
