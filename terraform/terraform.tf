1f6d2eb9f84a65a35fc1ce7829c0df1adaed1824
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-06-07 11:39:40 -0400

    Feature: adding kubernetes

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">=1.6"
   required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "3.106.1"
