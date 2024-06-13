8ed55182bff3a8d81b74a731fa144c56d375bccf
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-06-13 09:42:59 -0400

    adding k8s

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -4,22 +4,22 @@ data "azurerm_kubernetes_service_versions" "current" {
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-  location                          = azurerm_resource_group.azure_resource_group.location
   name                              = random_pet.admin_username.id
   sku_tier                          = "Standard"
   resource_group_name               = azurerm_resource_group.azure_resource_group.name
+  location                          = azurerm_resource_group.azure_resource_group.location
   dns_prefix                        = random_pet.admin_username.id
   kubernetes_version                = data.azurerm_kubernetes_service_versions.current.latest_version
   role_based_access_control_enabled = true
-  #  api_server_access_profile {
-  #  authorized_ip_ranges = [
-  #    "1.2.3.4/32"
-  ##  ]
+  api_server_access_profile {
+    authorized_ip_ranges = [
+      "1.2.3.4/32"
+    ]
 
-  #}
-  #oms_agent {
-  #  log_analytics_workspace_id = random_pet.admin_username.id
-  #}
+  }
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.log-analytics
+  }
 
   identity {
     type = "SystemAssigned"
@@ -27,15 +27,28 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   default_node_pool {
     temporary_name_for_rotation = "rotation"
-    name       = "agentpool"
-    vm_size    = "Standard_F16s_v2"
-    node_count = "1"
+    name                        = "agentpool"
+    vm_size                     = "Standard_F16s_v2"
+    node_count                  = "1"
   }
   network_profile {
     network_plugin    = "azure"
     network_policy    = "azure"
     load_balancer_sku = "standard"
   }
+}
+
+resource "azurerm_log_analytics_workspace" "log-analytics" {
+  name                = "log-analytics"
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
+  location            = azurerm_resource_group.azure_resource_group.location
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_kubernetes_cluster_extension" "flux-extension" {
+  name           = "flux-extension"
+  cluster_id     = azurerm_kubernetes_cluster.k8s.id
+  extension_type = "microsoft.flux"
 }
 
 output "kube_config" {
@@ -47,13 +60,4 @@ resource "local_file" "kubeconfig" {
   depends_on = [azurerm_kubernetes_cluster.k8s]
   filename   = "~/.kube/config"
   content    = azurerm_kubernetes_cluster.k8s.kube_config_raw
-}
-
-resource "helm_release" "argo-cd" {
-  depends_on = [azurerm_kubernetes_cluster.k8s]
-  name      = "argo-cd"
-  create_namespace = true
-  repository = "https://charts.bitnami.com/bitnami"
-  chart     = "argo-cd"
-  namespace = "argo-cd"
 }
