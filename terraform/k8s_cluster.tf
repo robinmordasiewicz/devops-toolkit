59ecf722e4dadcb4253727e03bccf391245d880c
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-07-16 13:39:49 -0400

    updated config

--- terraform/k8s_cluster.tf
+++ terraform/k8s_cluster.tf
@@ -3,28 +3,25 @@ data "http" "myip" {
 }
 
 data "azurerm_kubernetes_service_versions" "current" {
-  for_each        = local.kubernetes_clusters
-  location        = each.value.location
+  location        = azurerm_resource_group.azure_resource_group.location
   include_preview = false
 }
 
 resource "azurerm_log_analytics_workspace" "log_analytics" {
-  for_each            = local.kubernetes_clusters
-  name                = each.value.name
-  location            = each.value.location
-  resource_group_name = each.value.resource_group_name
+  name                = "log-analytics"
+  location            = azurerm_resource_group.azure_resource_group.location
+  resource_group_name = azurerm_resource_group.azure_resource_group.name
   sku                 = "PerGB2018"
 }
 
 resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
-  for_each                          = local.kubernetes_clusters
-  name                              = each.value.name
-  location                          = each.value.location
-  resource_group_name               = each.value.resource_group_name
-  dns_prefix                        = "${var.prefix}-aks-${each.key}"
-  kubernetes_version                = data.azurerm_kubernetes_service_versions.current[each.key].latest_version
+  name                              = "kubernetes_cluster"
+  location                          = azurerm_resource_group.azure_resource_group.location
+  resource_group_name               = azurerm_resource_group.azure_resource_group.name
+  dns_prefix                        = azurerm_resource_group.azure_resource_group.name
+  kubernetes_version                = data.azurerm_kubernetes_service_versions.current.latest_version
   sku_tier                          = "Standard"
-  node_resource_group               = "MC-${each.value.name}"
+  node_resource_group               = "MC-${azurerm_resource_group.azure_resource_group.name}"
   role_based_access_control_enabled = true
   oidc_issuer_enabled               = true
   workload_identity_enabled         = true
@@ -34,13 +31,13 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
     ]
   }
   oms_agent {
-    log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics[each.key].id
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
   }
   default_node_pool {
     temporary_name_for_rotation = "rotation"
-    name                        = each.value.default_node_pool.name
-    node_count                  = each.value.default_node_pool.node_count
-    vm_size                     = each.value.default_node_pool.vm_size
+    name                        = "default"
+    node_count                  = 1
+    vm_size                     = "Standard_B4ms"
     os_sku                      = "AzureLinux"
     upgrade_settings {
       max_surge = "10%"
@@ -53,15 +50,14 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
   }
 
   identity {
-    type = each.value.identity.type
+    type = "SystemAssigned"
   }
 }
 
 resource "azurerm_kubernetes_cluster_node_pool" "node-pool" {
-  for_each              = local.kubernetes_clusters
   name                  = "gpu"
   mode                  = "User"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.kubernetes_cluster[each.key].id
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.kubernetes_cluster.id
   depends_on            = [azurerm_kubernetes_cluster.kubernetes_cluster]
   vm_size               = "Standard_NC6s_v3" #16GB
   #vm_size               = "Standard_NC24s_v3"
@@ -70,7 +66,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "node-pool" {
   #vm_size               = "Standard_NC24ads_A100_v4" # 80GB
   node_count        = 1
   os_sku            = "AzureLinux"
-  node_taints       = var.user_node_pool_node_taints
+  node_taints       = ["nvidia.com/gpu=true:NoSchedule"]
   os_disk_type      = "Ephemeral"
   ultra_ssd_enabled = true
   os_disk_size_gb   = "256"
@@ -79,9 +75,8 @@ resource "azurerm_kubernetes_cluster_node_pool" "node-pool" {
 }
 
 resource "azurerm_kubernetes_cluster_extension" "flux_extension" {
-  for_each          = local.kubernetes_clusters
   name              = "flux-extension"
-  cluster_id        = azurerm_kubernetes_cluster.kubernetes_cluster[each.key].id
+  cluster_id        = azurerm_kubernetes_cluster.kubernetes_cluster.id
   extension_type    = "microsoft.flux"
   release_namespace = "flux-system"
   depends_on        = [azurerm_kubernetes_cluster_node_pool.node-pool]
@@ -98,14 +93,13 @@ data "git_repository" "current" {
 }
 
 resource "azurerm_kubernetes_flux_configuration" "flux_configuration" {
-  for_each                          = local.kubernetes_clusters
   name                              = "flux-configuration"
-  cluster_id                        = azurerm_kubernetes_cluster.kubernetes_cluster[each.key].id
+  cluster_id                        = azurerm_kubernetes_cluster.kubernetes_cluster.id
   namespace                         = "cluster-config"
   scope                             = "cluster"
   continuous_reconciliation_enabled = true
   git_repository {
-    url                      = var.manifest_url
+    url                      = "https://github.com/robinmordasiewicz/devops-toolkit"
     reference_type           = "branch"
     reference_value          = data.git_repository.current.branch
     sync_interval_in_seconds = 60
@@ -132,14 +126,13 @@ resource "azurerm_kubernetes_flux_configuration" "flux_configuration" {
 
 output "kube_config" {
   description = "kube config"
-  value       = [for cluster in azurerm_kubernetes_cluster.kubernetes_cluster : cluster.kube_config_raw]
+  value       = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config_raw
   sensitive   = true
 }
 
 resource "local_file" "kube_config" {
-  for_each             = local.kubernetes_clusters
-  content              = azurerm_kubernetes_cluster.kubernetes_cluster[each.key].kube_config_raw
-  filename             = "/home/vscode/.kube/${each.value.name}.yaml"
+  content              = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config_raw
+  filename             = "/home/vscode/.kube/config.yaml"
   directory_permission = "0755"
   file_permission      = "0600"
 }
