c71646cedb3e0f254de32adafbe0026ce6718156
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-08-04 14:15:14 -0400

    adding init

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -216,13 +216,13 @@ variable "hub-nva-management-action" {
   }
 }
 
-variable "spoke-linux-server-image" {
-  default     = "linux-server"
+variable "spoke-aks-node-image" {
+  default     = "aks-node"
   description = "Container server image product"
   type        = string
 }
 
-variable "spoke-linux-server-image-gpu" {
+variable "spoke-aks-node-image-gpu" {
   default     = false
   description = "Set to true to enable GPU workloads"
   type        = bool
@@ -319,12 +319,12 @@ variable "spoke-aks_dns_service_ip" {
   }
 }
 
-variable "spoke-linux-server-ip" {
-  default     = "10.1.1.5"
+variable "spoke-aks-node-ip" {
+  default     = "10.1.1.4"
   description = "Spoke Container Server IP Address"
   type        = string
   validation {
-    condition     = can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.spoke-linux-server-ip))
+    condition     = can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.spoke-aks-node-ip))
     error_message = "The IP address must be a valid IPv4 format (e.g., 10.1.1.5)."
   }
 }
@@ -339,13 +339,13 @@ variable "spoke-check-internet-up-ip" {
   }
 }
 
-variable "spoke-linux-server-ollama-port" {
+variable "spoke-aks-node-ollama-port" {
   default     = "11434"
   description = "Port for ollama"
   type        = string
 }
 
-variable "spoke-linux-server-ollama-webui-port" {
+variable "spoke-aks-node-ollama-webui-port" {
   default     = "8080"
   description = "Port for the ollama web ui"
   type        = string
