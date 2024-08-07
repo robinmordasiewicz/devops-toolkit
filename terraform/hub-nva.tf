3f256368c199f727563c4b1f5a3ca64ad3ddcd40
Robin Mordasiewicz <robin@mordasiewicz.com>
2024-08-07 15:10:03 -0400

    adding ks8

--- terraform/hub-nva.tf
+++ terraform/hub-nva.tf
@@ -93,3 +93,19 @@ resource "azurerm_linux_virtual_machine" "hub-nva_virtual_machine" {
     )
   )
 }
+
+resource "azurerm_managed_disk" "disk" {
+  name                 = "hub-nva-disk1"
+  location             = azurerm_resource_group.azure_resource_group.location
+  resource_group_name  = azurerm_resource_group.azure_resource_group.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 10
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "example" {
+  managed_disk_id    = azurerm_managed_disk.disk.id
+  virtual_machine_id = azurerm_linux_virtual_machine.hub-nva_virtual_machine.id
+  lun                = "0"
+  caching            = "ReadWrite"
+}
